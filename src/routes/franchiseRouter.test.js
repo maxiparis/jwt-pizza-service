3cdb0762158b0d5935eb796fd2934c0c96a1da80
Maximiliano Paris <mparis@hotmail.cl>
2025-02-05 09:06:47 -0700

    tests: 74.8

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -24,38 +24,83 @@ test("create-franchise-bad", async () => {
     expect(res.status).toBe(403);
 })
 
+describe("franchises",  () => {
+    let newFranchisee;
+    let adminUser;
+    let franchises;
 
-test("create-franchise-and-get-franchise", async () => {
-    /**
-     * Steps to create franchisee:
-     * Admin creates a franchise with a diner email. Once the email is validated successfully
-     * the diner is made a franchisee.
-     */
+    test("create-franchise", async() => {
+        /**
+         * Steps to create franchisee:
+         * Admin creates a franchise with a diner email. Once the email is validated successfully
+         * the diner is made a franchisee.
+         */
 
-    //register as admin
-    const adminUser = await registerRandomAdmin()
-    expect(adminUser.token).toBeDefined();
+        //register as admin
+        adminUser = await registerRandomAdmin()
+        expect(adminUser.token).toBeDefined();
 
-    //register a user
-    const diner = await registerRandomUser()
-    expect(diner.user.email).toBeDefined();
-    expect(diner.user.id).toBeDefined();
-    expect(diner.token).toBeDefined();
+        //register a user
+        newFranchisee = await registerRandomUser()
+        expect(newFranchisee.user.email).toBeDefined();
+        expect(newFranchisee.user.id).toBeDefined();
+        expect(newFranchisee.token).toBeDefined();
 
-    //creates franchise
-    let res = await request(app)
-        .post(`/api/franchise`)
-        .set('Authorization', `Bearer ${adminUser.token}`)
-        .send({name: "franchise " + generateRandomString(), admins: [{email: diner.user.email}]})
+        //creates franchise
+        let res = await request(app)
+            .post(`/api/franchise`)
+            .set('Authorization', `Bearer ${adminUser.token}`)
+            .send({name: "franchise " + generateRandomString(), admins: [{email: newFranchisee.user.email}]})
 
-    expect(res.status).toBe(200);
+        expect(res.status).toBe(200);
+    });
 
-    //gets franchises for user diner
-    res = await request(app)
-        .get(`/api/franchise/${diner.user.id}`)
-        .set('Authorization', `Bearer ${diner.token}`)
+    test("get-franchises", async () => {
+        let res = await request(app)
+            .get(`/api/franchise/${newFranchisee.user.id}`)
+            .set('Authorization', `Bearer ${newFranchisee.token}`)
 
-    expect(res.status).toBe(200);
-    expect(res._body.length).toBeGreaterThan(0)
+        expect(res.status).toBe(200);
+        expect(res._body.length).toBeGreaterThan(0)
 
+        franchises = res._body
+    });
+
+    test("create-store", async () => {
+        //TODO
+    })
+
+    test("create-store-bad", async () => {
+        //TODO
+    })
+
+    test("delete-store", async () => {
+        //TODO
+    })
+
+    test("delete-store-bad", async () => {
+        //TODO
+    })
+
+    test("delete-franchise-bad", async () => {
+        expect(newFranchisee.token).toBeDefined()
+
+        let res = await request(app)
+            .delete(`/api/franchise/12391239}`) //Bad id
+            .set('Authorization', `Bearer ${newFranchisee.token}`) //wrong token passed
+
+        expect(res.status).toBe(403);
+    })
+
+    test("delete-franchise", async () => {
+        expect(franchises[0].id).toBeDefined()
+
+        let res = await request(app)
+            .delete(`/api/franchise/${franchises[0].id}`)
+            .set('Authorization', `Bearer ${adminUser.token}`)
+
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('franchise deleted');
+    })
 })
+
